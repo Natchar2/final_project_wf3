20550d3d9149bd1b0fd20e5cbef560bb772aff66
belzebuk <gaylordsarrazin@hotmail.com>
2017-09-27 17:23:12 +0200

    ajout de categorie et produits dans les controleur

--- src/Application/Controller/InterfaceCommerceController.php
+++ src/Application/Controller/InterfaceCommerceController.php
@@ -1,40 +1,53 @@
 <?php
-	namespace Application\Controller;
+namespace Application\Controller;
 
-	use Silex\Application;
-	use Symfony\Component\HttpFoundation\Request;
-	use Symfony\Component\HttpFoundation\Response; 
-	use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response; 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-	class InterfaceCommerceController
+class InterfaceCommerceController
+{
+	public function accueilAction(Application $app)
 	{
-	    public function accueilAction(Application $app)
-	    {
-	    	return $app['twig']->render('accueil.html.twig');
-	    }
-
-	    public function panierAction(Application $app)
-	    {
-	    	return $app['twig']->render('panier.html.twig');
-	    }
-
-	    public function feedbackAction(Application $app, Request $request)
-	    {
-			$transport = $app['swiftmailer.transport'];
-			$mailer = $app['mailer'];
-
-			$message = (new \Swift_Message())
-				->setSubject('Subject Mail')
-				->setFrom(array('postmaster@localhost'))
-				->setTo(array('root@localhost'))
-				->setBody("This is an email");
-
-		    $app['swiftmailer.use_spool'] = false;
-	    	$result = $mailer->send($message);
-
-	    	return $app['twig']->render('feedback.html.twig');
-	    }
+		return $app['twig']->render('accueil.html.twig');
 	}
-	
+
+	public function panierAction(Application $app)
+	{
+		return $app['twig']->render('panier.html.twig');
+	}
+
+	public function categorieAction($libellecategorie,Application $app)
+	{
+
+		$articles = $app['idiorm.db']->for_table('products')->where('category_name', $category_name)->find_result_set();
+
+		return $app['twig']->render('categorie.html.twig',[
+			'products' => $products
+		]);
+	}
+
+
+
+
+	public function feedbackAction(Application $app, Request $request)
+	{
+		$transport = $app['swiftmailer.transport'];
+		$mailer = $app['mailer'];
+
+		$message = (new \Swift_Message())
+		->setSubject('Subject Mail')
+		->setFrom(array('postmaster@localhost'))
+		->setTo(array('root@localhost'))
+		->setBody("This is an email");
+
+		$app['swiftmailer.use_spool'] = false;
+		$result = $mailer->send($message);
+
+		return $app['twig']->render('feedback.html.twig');
+	}
+}
+
 
 ?>
--- src/Application/Provider/InterfaceCommerceControllerProvider.php
+++ src/Application/Provider/InterfaceCommerceControllerProvider.php
@@ -1,29 +1,34 @@
 <?php
-	namespace Application\Provider;
-	
-	use Silex\Api\ControllerProviderInterface;
-	use Silex\Application;
-	
-	class InterfaceCommerceControllerProvider implements ControllerProviderInterface
+namespace Application\Provider;
+
+use Silex\Api\ControllerProviderInterface;
+use Silex\Application;
+
+class InterfaceCommerceControllerProvider implements ControllerProviderInterface
+{
+	public function connect(Application $app)
 	{
-	    public function connect(Application $app)
-	    {
-	    	$controllers = $app['controllers_factory'];
+		$controllers = $app['controllers_factory'];
 
-	    	$controllers
-	    		->get('/',  function() use($app){
-	    			return $app->redirect('accueil');
-	    		});
+		$controllers
+		->get('/',  function() use($app){
+			return $app->redirect('accueil');
+		});
 
-	    	$controllers
-	    		->get('/accueil', 'Application\Controller\InterfaceCommerceController::accueilAction')
-	    		->bind('accueil');
+		$controllers
+		->get('/accueil', 'Application\Controller\InterfaceCommerceController::accueilAction')
+		->bind('accueil');
 
-	    	$controllers
-	    		->get('/panier', 'Application\Controller\InterfaceCommerceController::panierAction')
-	    		->bind('panier');
+		$controllers
+		->get('/panier', 'Application\Controller\InterfaceCommerceController::panierAction')
+		->bind('panier');
 
-	    	return $controllers;
-	    }
+		$controllers
+		->get('/categorie', 'Application\Controller\InterfaceCommerceController::categorieAction')
+		->bind('categorie');
+
+
+		return $controllers;
 	}
+}
 ?>
