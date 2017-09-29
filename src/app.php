793a1d43e86b2778cb9bd907ede6e66e4aaf2d46
Natchar <lallemandnathan@hotmail.fr>
2017-09-29 14:21:08 +0200

    ajout de assets et dist

--- src/app.php
+++ src/app.php
@@ -27,6 +27,10 @@
 	$app->register(new Silex\Provider\AssetServiceProvider());
 	#----------------------------------------------------------------------
 
+	# -- SESSION
+	$app->register(new Silex\Provider\SessionServiceProvider());
+	#----------------------------------------------------------------------
+
 	# -- SWIFT MAILER
 	$app->register(new Silex\Provider\SwiftmailerServiceProvider(), array(
 		'swiftmailer.use_spool' => false,
@@ -41,6 +45,35 @@
 
 	# -- CSRF
 	$app->register(new Silex\Provider\CsrfServiceProvider());
+	#----------------------------------------------------------------------
+
+	# -- PAYPAL
+	$apiContext = new \PayPal\Rest\ApiContext(
+        new \PayPal\Auth\OAuthTokenCredential(
+            'AbB8aZ2BYsMvDfrV9MFhOxkhnwgaUawUSka2QPVwNfOrU5CU7h6ubG9BRiSx3UQ8233Ip-XgxYGhvnkg',     // ClientID
+            'EHdrwuNnzTp98c6UpIaVpAYD00wBKiUNXNQXmWHUXDrKQJWSIi-Jx4Kz13h7gWJsjmuLWrKrKm7yOfqV'      // ClientSecret
+        )
+	);
+	// 3. Lets try to save a credit card to Vault using Vault API mentioned here
+	// https://developer.paypal.com/webapps/developer/docs/api/#store-a-credit-card
+	$creditCard = new \PayPal\Api\CreditCard();
+	$creditCard->setType("visa")
+	    ->setNumber("4020029809497131")
+	    ->setExpireMonth("10")
+	    ->setExpireYear("2022")
+	    ->setCvv2("000")
+	    ->setFirstName("Test")
+	    ->setLastName("John");
+	// 4. Make a Create Call and Print the Card
+	try {
+	    $creditCard->create($apiContext);
+	    echo $creditCard;
+	}
+	catch (\PayPal\Exception\PayPalConnectionException $ex) {
+	    // This will print the detailed information on the exception. 
+	    //REALLY HELPFUL FOR DEBUGGING
+	    echo $ex->getData();
+	}
 	#----------------------------------------------------------------------
 
 	# -- FORM
