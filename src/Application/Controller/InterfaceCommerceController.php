53029705593e5969cd1688f34f565bf8f3566331
haydee75 <haydee.leconte@gmail.com>
2017-10-02 15:07:41 +0200

    ajout des views et css

--- src/Application/Controller/InterfaceCommerceController.php
+++ src/Application/Controller/InterfaceCommerceController.php
@@ -14,106 +14,161 @@ class InterfaceCommerceController
 	use Shortcut;
 
 
-    public function accueilAction(Application $app)
-    {
-    	return $app['twig']->render('commerce/accueil.html.twig');
-    }
+  public function accueilAction(Application $app)
+  {
+   return $app['twig']->render('commerce/accueil.html.twig');
+ }
 
-    public function panierAction(Application $app)
-    {
-    	return $app['twig']->render('commerce/panier.html.twig');
-    }
+ public function panierAction(Application $app)
+ {
+   return $app['twig']->render('commerce/panier.html.twig');
+ }
 
-    public function addItemAction(Application $app, Request $request)
-    {
-    	return new Response($request->get('id'));
-    }
+ public function addItemAction(Application $app, Request $request)
+ {
+   return new Response($request->get('id'));
+ }
 
-	public function categorieAction($category_name,Application $app)
-	{
+ public function faqAction(Application $app)
+ {
+  return $app['twig']->render('commerce/FAQ.html.twig');
+}
 
-		$articles = $app['idiorm.db']->for_table('products')->where('category_name', $category_name)->find_result_set();
+public function aboutAction(Application $app)
+{
+  return $app['twig']->render('commerce/about.html.twig');
+}
 
-		return $app['twig']->render('categorie.html.twig',[
-			'products' => $products
-		]);
-	}
+public function ajoutArticleAction(Application $app)
+{
+  return $app['twig']->render('commerce/ajoutArticle.html.twig');
+}
 
-  public function newAdAction(Application $app){
+public function forumAjoutPostAction(Application $app)
+{
+  return $app['twig']->render('commerce/forumAjoutPost.html.twig');
+}
+
+public function forumIndexAction(Application $app)
+{
+  return $app['twig']->render('commerce/forumIndex.html.twig');
+}
+
+public function forumPostDetailAction(Application $app)
+{
+  return $app['twig']->render('commerce/forumPostDetail.html.twig');
+}
+
+public function itemAction(Application $app)
+{
+  return $app['twig']->render('commerce/item.html.twig');
+}
+
+public function shopAction(Application $app)
+{
+  return $app['twig']->render('commerce/shop.html.twig');
+}
+
+public function shoppingCardAction(Application $app)
+{
+  return $app['twig']->render('commerce/shoppingCard.html.twig');
+}
+
+public function connexionAction(Application $app)
+{
+  return $app['twig']->render('commerce/connexion.html.twig');
+}
+
+public function inscriptionAction(Application $app)
+{
+  return $app['twig']->render('commerce/inscription.html.twig');
+}
+
+public function categorieAction($category_name,Application $app)
+{
+
+  $articles = $app['idiorm.db']->for_table('products')->where('category_name', $category_name)->find_result_set();
+
+  return $app['twig']->render('categorie.html.twig',[
+   'products' => $products
+ ]);
+}
+
+public function newAdAction(Application $app){
 
 
-    return $app['twig']->render('commerce/ajout_produit.html.twig', [
-        'categories' => $app['categories'],      
-            'error'  => [] ,
-            'errors' => []
-         ]);
-  }
+  return $app['twig']->render('commerce/ajout_produit.html.twig', [
+    'categories' => $app['categories'],      
+    'error'  => [] ,
+    'errors' => []
+  ]);
+}
 
 
-  public function newAdPostAction(Application $app, Request $request){
+public function newAdPostAction(Application $app, Request $request){
 
 //utilisation de la fonction de vérification dans Model\Vérifications
-    $Verifications = new Verifications;
+  $Verifications = new Verifications;
 
-    $verifs =  $Verifications->VerificationNewAd($request, $app);
+  $verifs =  $Verifications->VerificationNewAd($request, $app);
 
-     
+
  //Retour des variables de VerificationNewAd
-    $errors         = $verifs['errors'];
-    $error          = $verifs['error'];
-    $finalFileName1 = $verifs['finalFileName1'];
-    $finalFileName2 = $verifs['finalFileName2'];
-    $finalFileName3 = $verifs['finalFileName3'];
+  $errors         = $verifs['errors'];
+  $error          = $verifs['error'];
+  $finalFileName1 = $verifs['finalFileName1'];
+  $finalFileName2 = $verifs['finalFileName2'];
+  $finalFileName3 = $verifs['finalFileName3'];
 
 
-    if(empty($errors) && empty($error)){
+  if(empty($errors) && empty($error)){
       //Connexion à la bdd
-    		$product = $app['idiorm.db']->for_table('products')->create();
+    $product = $app['idiorm.db']->for_table('products')->create();
 
 
 
     		//Affectation des valeurs
-    		$product->name 			   = $request->get('name');
-    		$product->brand    		 = $request->get('brand');
-    		$product->price 		   = $request->get('price');
-    		$product->description	 = $request->get('description');
-    		$product->image_1		   = $finalFileName1;
-    		$product->image_2		   = $finalFileName2;
-    		$product->image_3		   = $finalFileName3;
-        $product->ID_category  = $request->get('category');
-        $product->creation_date= strtotime('now');
+    $product->name 			   = $request->get('name');
+    $product->brand    		 = $request->get('brand');
+    $product->price 		   = $request->get('price');
+    $product->description	 = $request->get('description');
+    $product->image_1		   = $finalFileName1;
+    $product->image_2		   = $finalFileName2;
+    $product->image_3		   = $finalFileName3;
+    $product->ID_category  = $request->get('category');
+    $product->creation_date= strtotime('now');
 
           //Affectation d'une valeur par défaut à zéro si il n'y en a pas eu dans le formulaire
-        if((float)$request->get('shipping_charges') == 0.0){
+    if((float)$request->get('shipping_charges') == 0.0){
 
-          $product->shipping_charges = 0.0;
+      $product->shipping_charges = 0.0;
 
-        }else{
+    }else{
 
-    		  $product->shipping_charges  = $request->get('shipping_charges');
-      }
+      $product->shipping_charges  = $request->get('shipping_charges');
+    }
 
     		//ON persiste en bdd
-    		$product->save();
+    $product->save();
 
-        return $app['twig']->render('commerce/ajout_produit.html.twig',[
-                  'success'    => true,
-                  'errors'     => [] ,
-                  'error'      => [] ,
-                  'categories' => $app['categories']
-                ]);
+    return $app['twig']->render('commerce/ajout_produit.html.twig',[
+      'success'    => true,
+      'errors'     => [] ,
+      'error'      => [] ,
+      'categories' => $app['categories']
+    ]);
 
-    		}else{
+  }else{
 
-    			return $app['twig']->render('commerce/ajout_produit.html.twig',[
-    				'errors'    => $errors,
-    				'error'     => $error,
-                    'categories'=> $app['categories']
-    			]);
+   return $app['twig']->render('commerce/ajout_produit.html.twig',[
+    'errors'    => $errors,
+    'error'     => $error,
+    'categories'=> $app['categories']
+  ]);
 
-    		}
-    	
-  	}
+ }
+
+}
 
 
 }
