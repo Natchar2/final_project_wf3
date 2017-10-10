80302605f08e6259f83e4b1fa35e2e05c8eb9b6c
gouly <gaylordsarrazin@hotmail.com>
2017-10-10 16:52:57 +0200

    ca va bugger

--- src/Application/Provider/AdminControllerProvider.php
+++ src/Application/Provider/AdminControllerProvider.php
@@ -28,6 +28,11 @@
 			->bind('listProductsAdmin');
 
 			$controllers
+			->post('/produit/liste', 'Application\Controller\AdminController::deleteProductAdminAction')
+			->assert('ID_product', '\d+')
+			->bind('deleteProductAdmin');
+
+			$controllers
 			->get('/produit/ajouter/{ID_product}/{token}', 'Application\Controller\AdminController::addProductAdminAction')
 			->assert('ID_product', '\d+')
 			->value('ID_product', '0')
@@ -36,10 +41,13 @@
 			->bind('addProductAdmin');
 
 			$controllers
-			->post('/produit/ajouter/{ID_product}', 'Application\Controller\AdminController::addProductPostAdminAction')
-			->assert('ID_product', '\d+')
-			->value('ID_product', '0')
+			->post('/produit/ajouter', 'Application\Controller\AdminController::addProductPostAdminAction')
 			->bind('addProductPostAdmin');
+
+
+
+
+
 
 
 //--------------------------- Agenda --------------------------------------
@@ -47,6 +55,13 @@
 			$controllers
 			->get('/event/liste', 'Application\Controller\AdminController::listEventsAdminAction')
 			->bind('listEventsAdmin');
+
+
+			$controllers
+			->post('/event/liste', 'Application\Controller\AdminController::deleteEventAdminAction')
+			->bind('deleteEventAdmin');
+
+
 
 			$controllers
 			->get('/event/ajouter/{ID_event}/{token}','Application\Controller\AdminController::addEventAdminAction')
@@ -57,9 +72,7 @@
 			->bind('addEventAdmin');
 
 			$controllers
-			->post('/event/ajouter/{ID_event}','Application\Controller\AdminController::addEventPostAdminAction')
-			->assert('ID_event', '\d+')
-			->value('ID_event', '0')
+			->post('/event/ajouter','Application\Controller\AdminController::addEventPostAdminAction')
 			->bind('addEventPostAdmin');
 
 
@@ -73,7 +86,7 @@
 
 			$controllers
 			->post('/topic/liste', 'Application\Controller\AdminController::deleteTopicAdminAction')
-			->bind('deleteTopic');
+			->bind('deleteTopicAdmin');
 
 			$controllers
 			->get('/topic/ajouter', 'Application\Controller\AdminController::addTopicAdminAction')
